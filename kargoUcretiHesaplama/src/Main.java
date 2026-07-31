820ea1fe4994e5b7c0989501aef6f33479fcfe97
Sude <sudeky3834@gmail.com>
2026-07-31 20:51:57 +0300

    Add implementation

--- kargoUcretiHesaplama/src/Main.java
+++ kargoUcretiHesaplama/src/Main.java
@@ -1,10 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+
+        double kargoUcreti;
+        double tutar;
+        int mesafe;
+
+        do {
+            System.out.println("Sipariş tutarı: ");
+             tutar = scanner.nextDouble();
+
+        }while(tutar<=0);
+
+        do {
+            System.out.println("Teslimat mesafesi: ");
+            mesafe = scanner.nextInt();
+
+        }while(mesafe<=0);
+
+        System.out.println("Premium üye misiniz?(true/false) ");
+        boolean premium = scanner.nextBoolean();
+
+        if(tutar>=1000)
+        {
+            System.out.println("Kargo ücretsiz");
+            return;
+        }
+
+        if (mesafe <= 50) {
+            kargoUcreti = 50;
+        }
+        else if (mesafe <= 150) {
+            kargoUcreti = 90;
+        }
+        else {
+            kargoUcreti = 150;
+        }
+
+        if (premium) {
+            kargoUcreti *= 0.5;
+        }
+
+        System.out.println("Kargo ücretiniz: " + kargoUcreti + " TL");
 
     }
 }
